734d8bd3293814305c845f990d13907b82bfa775
Ziyi Feng <ziyifeng1109@gmail.com>
2015-04-09 20:50:42 +1200

    create macros for semaphore functions for unix

--- rain.c
+++ rain.c
@@ -11,18 +11,28 @@
 //#define setColor(h,c) SetConsoleTextAttribute((hHandle), (c))
 #define setColor(c) SetConsoleTextAttribute((hHandle), (c))
 #define GOTO_XY(x,y) SetConsoleCursorPosition((hHandle), (COORD){(x),(y)})
-#define PRINT_GREEN(x) setColor(GREEN);printf("x\n");
-#define sem_wait(sem) WaitForSingleObject(sem,MAXLONG)
-#define sem_signal(sem) ReleaseSemaphore(sem,1,NULL)
-#define sem_create(n) CreateSemaphore(NULL,n,MAXLONG,NULL);
-#define semaphore HANDLE
+#define PRINT_GREEN(x) setColor(GREEN);printf("x\n")
+#define SEM_WAIT(sem) WaitForSingleObject(sem,MAXLONG)
+#define SEM_SIGNAL(sem) ReleaseSemaphore(sem,1,NULL)
+#define SEM_CREATE(n,m) n=CreateSemaphore(NULL,m,MAXLONG,NULL)
+#define SEMAPHORE HANDLE
 
 HANDLE hHandle;
 
-#elif __linux
+#elif __unix
 
-#define GOTO_XY(x,y)
-#define PRINT_GREEN(x) printf("\033[40;32mx\033[5m");
+#include <semaphore.h>
+
+#define SEM_NAME shared_sem
+#define SEMAPHORE sem_t
+#define SEM_CREATE(n,m) sem_init(n,0,m)
+#define SEM_SIGNAL(s) sem_post(s)
+#define SEM_WAIT(s) sem_wait(s)
+
+#define GOTO_XY(x,y) printf("\033[%d;%dH", x, y)
+#define PRINT_GREEN(x) printf("\033[40;32m%c\033[5m", x);
+
+sem_t SEM_NAME;
 
 #endif
 
@@ -66,8 +76,8 @@ int main(int argc, char *argv[]){
 	width_max = coord.X; 
 	height_max = coord.Y;
 
-	main_sem = sem_create(0);
-	mutex = sem_create(1);
+	SEM_CREATE(main_sem, 0);
+	SEM_CREATE(mutex, 1);
 
     clear();
 	for(i = 0; i < width_max; i++){
@@ -77,7 +87,7 @@ int main(int argc, char *argv[]){
 		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)rain_thread, (void*)r, 0, NULL);
 	}
 
-	sem_wait(main_sem);
+	SEM_WAIT(main_sem);
 
 	return 0;
 }
@@ -105,66 +115,81 @@ void* rain_thread(void *p){
 		j = rand() % CE;
 		if(j < CS) j += CS;
 		for(i = t.start; i< t.end+RAIN_LEN; i++){
-			sem_wait(mutex);
+			SEM_WAIT(mutex);
 			if(i < t.end){
 				//gotoxy(handle, t.pos, i);
 				//setColor(handle, 7);
 				//printf("%c", c1);
                 GOTO_XY(handle, t.pos, i);
-                PRINT_GREEN(c1);
+                PRINT_COLOR(c1, 7);
 
 				c4 = c3; c3 = c2; c2 = c1; c1 = j;
 				j = rand() % CE;;
 				if(j < CS) j += CS;
 
 				if(i > 0){
-					setColor(handle, 10);
-					gotoxy(handle, t.pos, i-1);
-					printf("%c", c2);
+					//setColor(handle, 10);
+					//gotoxy(handle, t.pos, i-1);
+					//printf("%c", c2);
+                    GOTO_XY(t.pos, i-1);
+                    PRINT_COLOR(c2, 10);
 				}
 				if(i > 1){
-					setColor(handle, 10);
-					gotoxy(handle, t.pos, i-2);
-					printf("%c", c3);
+					//setColor(handle, 10);
+					//gotoxy(handle, t.pos, i-2);
+					//printf("%c", c3);
+                    GOTO_XY(t.pos, i-2);
+                    PRINT_COLOR(c3, 10);
 				}
 				if(i > 2){
-					setColor(handle, 2);
-					gotoxy(handle, t.pos, i-3);
-					printf("%c", c4);
+					//setColor(handle, 2);
+					//gotoxy(handle, t.pos, i-3);
+					//printf("%c", c4);
+                    GOTO_XY(t.pos, i-3);
+                    PRINT_COLOR(c4, 2);
 				}
 			}
 
 			if(i == t.end){
-				setColor(handle, 10);
-				gotoxy(handle, t.pos, i-1);
-				printf("%c", c1);
-				setColor(handle, 2);
-				gotoxy(handle, t.pos, i-3);
-				printf("%c", c3);
+				//setColor(handle, 10);
+				//gotoxy(handle, t.pos, i-1);
+				//printf("%c", c1);
+                GOTO_XY(t.pos, i-1);
+                PRINT_COLOR(c1, 10);
+				//setColor(handle, 2);
+				//gotoxy(handle, t.pos, i-3);
+				//printf("%c", c3);
+                GOTO_XY(t.pos, i-3);
+                PRINT_COLOR(c3, 2);
 			}
 			if(i == (t.end+1)){
-				setColor(handle, 2);
-				gotoxy(handle, t.pos, i-3);
-				printf("%c", c2);
+				//setColor(handle, 2);
+				//gotoxy(handle, t.pos, i-3);
+				//printf("%c", c2);
+                GOTO_XY(t.pos, i-3);
+                PRINT_COLOR(c2, 2);
 			}
 			if(i == (t.end+2)){
-				setColor(handle, 2);
-				gotoxy(handle, t.pos, i-3);
-				printf("%c", c1);
+				//setColor(handle, 2);
+				//gotoxy(handle, t.pos, i-3);
+				//printf("%c", c1);
+                GOTO_XY(t.pos, i-3);
+                PRINT_COLOR(c1, 2);
 			}
 	
 			if(i >= RAIN_LEN){
-				gotoxy(handle, t.pos, i-RAIN_LEN);
+				//gotoxy(handle, t.pos, i-RAIN_LEN);
+                GOTO_XY(t.pos, i-RAIN_LEN);
 				printf(" ");
 			}
 
-			sem_signal(mutex);
+			SEM_SIGNAL(mutex);
 	
 			Sleep(50);
 		}
-		sem_wait(mutex);
+		SEM_WAIT(mutex);
 		rain_randomise(&t);
-		sem_signal(mutex);
+		SEM_SIGNAL(mutex);
 		//tmpdelay = rand() % t.pos * 100;
 	}
 }
@@ -178,7 +203,7 @@ void clear(){
 }
 
 void signal_handler(int num){
-    sem_wait(mutex);
+    SEM_WAIT(mutex);
     clear();
     gotoxy(handle, 0,0);
     setColor(handle, 7);
